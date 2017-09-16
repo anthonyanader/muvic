eaa95ce17bc0cd1b1b22039045940e73670e277d
anthonyanader <anthonyanader@gmail.com>
2017-09-16 10:47:42 -0400

    Changing some form scripts

--- js/main.js
+++ js/main.js
@@ -24,17 +24,14 @@ $(document).ready(function () {
     });
 
     zdrop.on('dragenter', function () {
-        // $('#zdrop').css({ background: '#41ab6b', color: '#fff' });
-        $('#zdrop').addClass('green lighten-1 white-text');
+        $('#zdrop').addClass('red lighten-1 white-text');
     });
 
     zdrop.on('dragleave', function () {
-        // $('#zdrop').css({ background: '#fff', color: '#000' });
         $('#zdrop').addClass('white black-text').removeClass('green lighten-1 white-text');
     });
 
     zdrop.on('drop', function () {
-        //  $('#zdrop').css({ background: '#fff', color: '#333' });
         $('#zdrop').addClass('white black-text').removeClass('green lighten-1 white-text');
     });
 });
