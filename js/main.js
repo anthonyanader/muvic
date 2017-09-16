175745f1e3e056d9fdbbaadad292394183aa7fb0
anthonyanader <anthonyanader@gmail.com>
2017-09-16 10:08:10 -0400

    Added code for DropZone.js. User can now upload any file by dropping or browsing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
 $(document).ready(function () {
   $(".button-collapse").sideNav();
+
+    var previewNode = document.querySelector("#zdrop-template");
+    previewNode.id = "";
+    var previewTemplate = previewNode.parentNode.innerHTML;
+    previewNode.parentNode.removeChild(previewNode);
+
+      var zdrop = new Dropzone("#zdrop", {
+            url: '/Home/UploadFile',
+            maxFilesize:20,
+            previewTemplate: previewTemplate,
+            autoQueue: false,
+            previewsContainer: "#previews",
+            clickable: "#zdrop"
+    });
+
+      zdrop.on("totaluploadprogress", function (progress) {
+        var progr = document.querySelector(".progress .determinate");
+        if (progr === undefined || progr === null)
+            return;
+
+        progr.style.width = progress + "%";
+    });
+
+    zdrop.on('dragenter', function () {
+        // $('#zdrop').css({ background: '#41ab6b', color: '#fff' });
+        $('#zdrop').addClass('green lighten-1 white-text');
+    });
+
+    zdrop.on('dragleave', function () {
+        // $('#zdrop').css({ background: '#fff', color: '#000' });
+        $('#zdrop').addClass('white black-text').removeClass('green lighten-1 white-text');
+    });
+
+    zdrop.on('drop', function () {
+        //  $('#zdrop').css({ background: '#fff', color: '#333' });
+        $('#zdrop').addClass('white black-text').removeClass('green lighten-1 white-text');
+    });
 });
