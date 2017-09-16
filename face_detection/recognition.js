f9c6cd7c865716919d6ac092f7e895a168261d06
Krista Shuckerow <kshuckerow@gmail.com>
2017-09-16 05:23:45 -0400

    fixed movePage variable to booleans

--- face_detection/recognition.js
+++ face_detection/recognition.js
@@ -28,19 +28,18 @@ detector.addEventListener("onWebcamConnectFailure", function() {
   alert("Webcam access denied");
 });
 
-
+//post to turn music page or not
 detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp){
-  //console.log(faces[0].expressions.browRaise);
 if(faces.length > 0 && (Number(faces[0].expressions.browRaise)).toFixed(0)>= 99){
   console.log((Number(faces[0].expressions.browRaise)).toFixed(0));
   $.post("server.js",
   {
-    movePage: "true"
-  })
+    movePage: true
+    })
 }else{
   $.post("server.js",
   {
-    movePage: "false"
+    movePage: false
   })
 }
 });
