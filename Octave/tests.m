828e120fda9fa80dcab1f3a62bbd654fb11c9209
pwstegman <pwstegman@gmail.com>
2017-09-16 01:28:29 -0400

    Simple FFT

--- Octave/tests.m
+++ Octave/tests.m
@@ -2,9 +2,25 @@
 % Let's collect a sample
 % Also, this looks neat: https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data
 
-% 8Khz recording
-y = record(5, 8000);
+pkg load signal;
 
-% Play the sample
-player = audioplayer(y, 8000);
-play(player);
+% 8Khz 2 second recording of A4
+% fs = 8000;
+% t = 2;
+% y = record(t, fs);
+% save -mat A4.mat y
+
+load('A4.mat');
+
+length = 2^nextpow2(numel(y));
+fs = 8000;
+f = fft(y, length);
+cutoff = floor(numel(f) / 2);
+p = abs(f(1:cutoff));
+x = fs*(0:cutoff-1)/length;
+plot(x', p);
+
+[pks, loc, extra] = findpeaks(p, 'DoubleSided');
+hold on;
+plot(fs*(loc .- 1)/length, pks, 'b.', 'MarkerSize', 20);
+hold off;
