57686c65c3dce7df7132184c0bf31dcc0329fb47
Wilson Wang <xerodragon72@gmail.com>
2017-09-16 13:56:33 -0400

    ITS BEAUTIFUL

--- js/face.js
+++ js/face.js
@@ -30,7 +30,8 @@ detector.addEventListener("onWebcamConnectFailure", function () {
 
 //post to turn music page or not
 detector.addEventListener("onImageResultsSuccess", function (faces, image, timestamp) {
-    if (faces.length > 0 && (Number(faces[0].expressions.browRaise)).toFixed(0) >= 95) {
+    console.log((Number(faces[0].expressions.browRaise)).toFixed(0));
+    if (faces.length > 0 && (Number(faces[0].expressions.browRaise)).toFixed(0) >= 90) {
         console.log((Number(faces[0].expressions.browRaise)).toFixed(0));
         window.scrollTo(0, 1200); 
     }
