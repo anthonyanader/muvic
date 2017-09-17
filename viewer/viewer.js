ca8f5dbc29f5ab0d9399cb259f30220e7451cb83
pwstegman <pwstegman@gmail.com>
2017-09-17 06:09:42 -0400

    Tracks measure

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -62,7 +62,7 @@ function doMetronome() {
         clearInterval(tickInterval);
     } else {
         tickInterval = setInterval(function () {
-            audio.play();
+            tick();
         }, 60000/bpm);
         $("#toolbar-metronome").text("Disable Metronome");
     }
@@ -70,14 +70,79 @@ function doMetronome() {
     $("#metronome").toggle('fast');
 }
 
+currentMeasure = 0;
+mCount = 0;
+tracking = false;
+function doTrack() {
+    $("#bluebox").toggle("fast");
+    tracking = !tracking;
+    canScroll = true;
+}
+
+function tick() {
+    audio.play();
+    if (tracking) {
+        mCount += 1;
+        if (mCount == 4) {
+            currentMeasure += 1;
+            mCount = 0;
+            highlightMeasure(currentMeasure);
+        }
+    }
+}
+
 function updateBPM(event) {
     bpm = $("#bpmRange").val();
     clearInterval(tickInterval);
     tickInterval = setInterval(function () {
-        audio.play();
+        tick();
     }, 60000 / bpm);
 }
 
-function highlightMeasure() {
+function highlightMeasure(m) {
+    var height = 110;
 
+    var offset = 0;
+
+    if (m == 0) {
+        r = 0;
+    } else {
+        var rows = VexFormatter.measuresInBlock;
+        found = false;
+        for (var r = 0; r < rows.length; r++) {
+            offset = 0;
+            for (var j = 0; j < rows[r].length; j++) {
+                if (rows[r][j] == m) {
+                    found = true;
+                    break;
+                } else {
+                    offset += VexFormatter.measureWidth[rows[r][j]] - 82;
+                }
+            }
+            if (found) {
+                break;
+            }
+        };
+    }
+
+    var sheetOffset = $("#sheet_canvas" + r).offset();
+    var startx = sheetOffset.left;
+    var starty = sheetOffset.top;
+
+    width = VexFormatter.measureWidth[m] - 82;
+
+    var top = starty + 35;
+    var left = startx + offset;
+
+    $("#bluebox").css({ top: top + "px", left: left + "px", width: width + "px", height: height + "px" });
+
+    if (canScroll) {
+        $('html, body').animate({
+            scrollTop: $("#bluebox").offset().top - 100
+        }, 2000);
+    }
 }
+
+$(window).scroll(function () {
+    canScroll = false;
+});
